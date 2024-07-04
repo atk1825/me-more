749e5d51fee89e05c98dfa3a3fbd36bb743bdccd
atk1825 <akkun1825@yahoo.co.jp>
2024-07-04 19:37:06 +0900

    モデルを見やすいように変更

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,12 @@
 class Post < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
+  validates :title, :content, presence: true
+  validates :genre_id, numericality: { other_than: 1 }
+
   belongs_to :genre
   belongs_to :user
   has_many :likes
-  validates :title, :content, presence: true
-  validates :genre_id, numericality: { other_than: 1 }
+
   def liked_by?(user)
     likes.where(user_id: user.id).exists? 
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :name, :age, presence: true
+
   has_many :posts
   has_many :likes
   has_one_attached :image
