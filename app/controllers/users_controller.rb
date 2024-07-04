26cb32c9bfce4737eb786cb9ece607a76bd7cffb
atk1825 <akkun1825@yahoo.co.jp>
2024-07-04 19:36:05 +0900

    投稿を保存する機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,20 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user! 
+  
   def index
     @posts = Post.all
     @users = User.all
+    if user_signed_in?
+      likes(current_user)
+    else
+      @like_posts = Post.none
+    end
+  end
+
+  private
+
+  def likes(user)
+    likes = Like.where(user_id: user.id).pluck(:post_id)
+    @like_posts = Post.where(id: likes)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "users#index"
-  resources :users, only: [:index]
+  resources :users, only: [:index] do
+    member do
+      get :likes
+    end
+  end
   resources :posts do
     resources :likes, only: [:create, :destroy]
   end
