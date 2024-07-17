3c0cab5301b1d87e84e04b6871142c06de804e2d
atk1825 <akkun1825@yahoo.co.jp>
2024-07-17 21:16:55 +0900

    アウトプットに画像を追加できるように修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -30,6 +30,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :content, :genre_id).merge(user_id: current_user.id)
+    params.require(:post).permit(:title, :content, :genre_id, :image).merge(user_id: current_user.id)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -6,12 +6,13 @@ class Post < ApplicationRecord
   belongs_to :genre
   belongs_to :user
   has_many :likes
+  has_one_attached :image
 
   def liked_by?(user)
     likes.where(user_id: user.id).exists?
   end
 
-  scope :yesterday, ->(user_id) { where(user_id: user_id, created_at: 1.day.ago.all_day) }
-  scope :three_days_later, ->(user_id) { where(user_id: user_id, created_at: 3.day.ago.all_day) }
-  scope :seven_days_later, ->(user_id) { where(user_id: user_id, created_at: 7.day.ago.all_day) }
+  scope :yesterday, ->(user_id) { where(user_id:, created_at: 1.day.ago.all_day) }
+  scope :three_days_later, ->(user_id) { where(user_id:, created_at: 3.day.ago.all_day) }
+  scope :seven_days_later, ->(user_id) { where(user_id:, created_at: 7.day.ago.all_day) }
 end
