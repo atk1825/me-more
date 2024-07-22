8df885249027d7686209446819c65c5a18628a82
atk1825 <akkun1825@yahoo.co.jp>
2024-07-22 21:02:18 +0900

    chatGPTの導入してみる

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  require 'openai'
+
   def index
     @posts = Post.all
   end
@@ -18,6 +20,7 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+    @problem = generate_problem(@post.content)
   end
 
   def destroy
@@ -31,5 +34,20 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:title, :content, :genre_id, :image).merge(user_id: current_user.id)
+  end
+
+  def generate_problem(content)
+    client = OpenAI::Client.new(api_key: ENV['OPENAI_API_KEY'])
+    response = client.completions(
+      parameters: {
+        model: 'gpt-3.5',
+        messages: [
+          { role: 'system', content: 'You are a helpful assistant.' },
+          { role: 'user', content: "Generate a problem based on the following content: #{content}" }
+        ],
+        max_tokens: 100
+      }
+    )
+    response.dig('choices', 0, 'message', 'content').strip
   end
 end
