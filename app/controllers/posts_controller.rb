2aedd9ee9cc1fe543a3cf7505612bbee911ab320
atk1825 <akkun1825@yahoo.co.jp>
2024-07-27 17:27:22 +0900

    チャットGPTの導入に成功！！！！

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  require 'openai'
+
   def index
     @posts = Post.all
   end
@@ -18,6 +20,8 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+    @client = OpenAI::Client.new(api: ENV['OPENAI_API_KEY'])
+    @problem = generate_problem(@post.content)
   end
 
   def destroy
@@ -31,5 +35,24 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:title, :content, :genre_id, :image).merge(user_id: current_user.id)
+  end
+
+  def generate_problem(content)
+    response = @client.chat(parameters: {
+                              model: 'gpt-3.5-turbo',
+                              messages: [
+                                { role: 'system', content: 'You are a helpful assistant.' },
+                                { role: 'user', content: "小学生向けに以下の内容に基づいた問題を作ってください。ただし問題の答えは提示しないでください: #{content}" }
+                              ],
+                              max_tokens: 200
+                            })
+    if response['choices'] && response['choices'][0] && response['choices'][0]['message'] && response['choices'][0]['message']['content']
+      response.dig('choices', 0, 'message', 'content').strip
+    else
+      'Error generating problem'
+    end
+  rescue StandardError => e
+    Rails.logger.error("OpenAI API Error: #{e.message}")
+    'Error generating problem'
   end
 end
